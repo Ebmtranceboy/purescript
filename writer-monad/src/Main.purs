0e0e22e107eda1bfdec0c0941a1cdd5ed7d2a7e4
Elodie Vella <elodievella@air-de-elodie.home>
2019-07-15 16:04:38 +0200

    log functionality

--- writer-monad/src/Main.purs
+++ writer-monad/src/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
 
-import Control.Monad.Writer(Writer, execWriter)
+import Control.Monad.Writer(Writer, execWriter, runWriter)
 import Control.Monad.Writer.Class(tell)
 
 import Data.Traversable(traverse_)
@@ -18,6 +18,17 @@ sumWriter arr = traverse_ (tell <<< Additive) arr
 sumArray :: Array Int -> Int
 sumArray arr = unwrap $ execWriter $ sumWriter arr
 
+collatzWriter :: Int -> Writer (Array Int) Int
+collatzWriter n = do
+  tell [n]
+  if n == 1 
+    then pure 0
+    else 
+      if n `mod`2 == 0 
+        then (_+1) <$> collatzWriter (n `div`2)
+        else (_+1) <$> collatzWriter (3*n+1)
+
 main :: Effect Unit
 main = do
   logShow $ sumArray [1,3,6,9]
+  logShow $ runWriter $ collatzWriter 10
